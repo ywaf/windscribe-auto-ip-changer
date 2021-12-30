9cbb8c9f81a2d284a81938149a813a10c7a3aea7
Leho <leho@leho.dev>
2021-12-30 14:44:58 +1100

    oops

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ while True:
     print("rotating ip now")
     randregion = random.choice(regions)
     print(randregion)
-    os.system('cd "C:\Program Files (x86)\Windscribe" && windscribe-cli.exe connect Sydney')
+    os.system('cd "C:\Program Files (x86)\Windscribe" && windscribe-cli.exe connect ' + str(randregion))
     print("connected at " + str(currenttime))
     print("waiting " + str(timetowait) + " minutes")
     print("-----------------------------------------")
